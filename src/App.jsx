93a56067130d1c672ea4a087ce04833df41d8a0b
Marcus Oliveira <marcus.oliveira@base.digital>
2022-08-07 00:00:21 -0300

    Git: Init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Post } from "./Post"
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from './App.module.css'
+
+import './global.css'
+
+export function App() {
+  return (
+    <div className="App">
+      <Header/>
+    
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
+    </div>
+  )
+}
